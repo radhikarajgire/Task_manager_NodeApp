d5891c50e3f4b4142b3e46130f2d195b0e8cbabe
radhikarajgire <58374458+radhikarajgire@users.noreply.github.com>
2020-12-30 23:20:20 +0100

    adding up models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,79 +1,32 @@
 const mongoose = require('mongoose')
-const validator = require ('validator')
+
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true 
 })
 
-const User = mongoose.model('User' , {
-    name: {
-       type: String,
-       required: true,
-       trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('passsword')) {
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
 
-const me = new User({
-    name: 'Rads',
-    email:'radsjj@gmail.com',
-    password:'  hdgsfisf'
-})
+// const me = new User({
+//     name: 'Rads',
+//     email:'radsjj@gmail.com',
+//     password:'  hdgsfisf'
+// })
 
-me.save().then(() => {
-console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then(() => {
+// console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
 
-const task = new Task({
-    description: 'Eat lunch'
-})
+// const task = new Task({
+//     description: 'Eat lunch'
+// })
 
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error', error)
-})
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Error', error)
+// })
