5c1c204a8710f8a8479b213aa0a9776206f243d3
radhikarajgire <58374458+radhikarajgire@users.noreply.github.com>
2021-01-03 17:18:40 +0100

    adding user and tasks routes

--- src/index.js
+++ src/index.js
@@ -1,103 +1,40 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
-
+// const User = require('./models/user')
+// const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-   const user = new User(req.body)
-   try{
-    await user.save()
-    res.status(201).send(user)
-   } catch(e) {
-    res.status(400).send(e)
-   }
-
-//    user.save().then(() => {
-//        res.status(201).send(user)
-//    }).catch((e) => {
-//        res.status(400).send(e)
-//    })
-
-})
-
-app.get('/users', async (req,res) => {
-    try {
-   const users = await User.find({})
-   res.send(users)
-    } catch(e) {
-   res.status(500).send()
-    }
-
-//     User.find({}).then((users) => {
-//        res.send(users)
-//     }).catch((e) => {
-//        res.status(500).send(e)
-//     })
- })
-
-app.get('/users/:id', async (req, res) => {
-    const _id= req.params.id
-try {
-    const user = await User.findById(_id)
-    if (!user) {
-        return res.status(404).send()
-    }
-    res.send(user)
-} catch (e) {
-    res.status(500).send(e)
-}
-
-//    User.findById(_id).then((user) => {
-//     if (!user) {
-//         return res.status(400).send()
-//     }
-//     res.send(user)
-//    }).catch((e) => {
-//        res.status(500).send(e)
-//    })
-})
-
-app.post('/tasks', async (req, res) => {
-const task = new Task(req.body)
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-   try {
-        const tasks = await Task.find({})
-        res.send (tasks)
-    }
-    catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/tasks/:id',async (req, res) => {
-    const _id = req.params.id
-   try {
-       const task = await Task.findById(_id) 
-       if (!task) {
-           return res.status(404).send()
-}
-res.send(task)
-   }
-    catch(e) {
-        res.status(500).send()
-    }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 
+// const router = new express.Router()
+// router.get('/test', (req, res) => {
+//     res.send('This is from my other router')
 
+// })
+// app.use(router)
 app.listen(port, () => {
     console.log('Server is on port' + port)
 })
+
+//hashing of password
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+const password = 'Red12345'
+const hashedPassword = await bcrypt.hash(password, 8)
+
+console.log(password)
+console.log(hashedPassword)
+
+//find given password is same as hashed one
+const isMatch = await bcrypt.compare('Red12345', hashedPassword)
+console.log(isMatch)
+}
+
+myFunction()
